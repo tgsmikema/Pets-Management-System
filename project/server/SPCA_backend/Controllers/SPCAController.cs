68052eaaac404e9861a832fd479470336ade4382
sma148 <sma148@aucklanduni.ac.nz>
2023-03-18 13:57:01 +1300

    added controller template

---
+++ project/server/SPCA_backend/Controllers/SPCAController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SPCA_backend.Data;
+using SPCA_backend.Model;
+
+namespace SPCA_backend.Controllers
+{
+    [Route("webapi")]
+    [ApiController]
+    public class SPCAController : Controller
+    {
+       private readonly ISPCARepo _repository;
+
+       public SPCAController(ISPCARepo repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("demo")]
+        public ActionResult demoFunction()
+        {
+            return Ok();
+        }
+    }
+}
