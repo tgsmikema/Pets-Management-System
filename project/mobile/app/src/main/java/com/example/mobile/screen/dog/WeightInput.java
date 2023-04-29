9f99660be644387489fcbbf22e210110c1ae7e48
bjur781 <79810883+bjur781@users.noreply.github.com>
2023-04-29 13:34:21 +1200

    Weight UI fix (#28)
    * web intermediate screen
    
    * mobile update

--- project/mobile/app/src/main/java/com/example/mobile/screen/dog/WeightInput.java
+++ project/mobile/app/src/main/java/com/example/mobile/screen/dog/WeightInput.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,18 +16,19 @@ import androidx.fragment.app.Fragment;
 import com.example.mobile.R;
 import com.example.mobile.databinding.FragmentEditDogBinding;
 import com.example.mobile.databinding.FragmentWeightBinding;
+import com.example.mobile.databinding.FragmentWeightInputBinding;
 import com.example.mobile.model.Dog;
 import com.example.mobile.screen.profile.ProfileFragment;
 
-public class Weight extends Fragment {
+public class WeightInput extends Fragment {
 
     private TextView subtitle;
     private Spinner locationSpinner;
     private Button cancelButton;
-    private Button startButton;
-    private FragmentWeightBinding binding;
+    private Button saveButton;
+    private FragmentWeightInputBinding binding;
 
-    public Weight() {
+    public WeightInput() {
         // Required empty public constructor
     }
 
@@ -36,7 +36,7 @@ public class Weight extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentWeightBinding.inflate(inflater, container, false);
+        binding = FragmentWeightInputBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         Bundle bundle = getArguments();
@@ -46,50 +46,20 @@ public class Weight extends Fragment {
         String dogLocation = bundle.getString("location");
         String dogWeight = bundle.getString("weight");
         String dogDate = bundle.getString("date");
+        String location = bundle.getString("location");
         boolean dogAlert = bundle.getBoolean("alert");
         boolean dogFlagged = bundle.getBoolean("flagged");
 
-        locationSpinner = root.findViewById(R.id.weight_location_spinner);
-        cancelButton = root.findViewById(R.id.weight_cancel);
-        startButton = root.findViewById(R.id.weight_start);
+        subtitle = root.findViewById(R.id.weight_input_subtitle);
+        subtitle.setText("Place " + dogName + " on " + location);
+        cancelButton = root.findViewById(R.id.weight_input_cancel);
+        saveButton = root.findViewById(R.id.weight_input_save);
 
-        //drop down menu to select location
-        locationSpinner = root.findViewById(R.id.weight_location_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.centers_list, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        locationSpinner.setAdapter(adapter);
-        final String[] selectedLocation = {""};
-
-        locationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                // Get the selected item from the spinner
-                selectedLocation[0] = parent.getItemAtPosition(position).toString();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                // Do nothing
-            }
-        });
-
-        startButton.setOnClickListener(new View.OnClickListener() {
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                WeightInput weightInput = new WeightInput();
-                Bundle bundle = new Bundle();
-                bundle.putString("name", dogName);
-                bundle.putString("breed", dogBreed);
-                bundle.putString("id", dogId);
-                bundle.putString("location", dogLocation);
-                bundle.putString("weight", dogWeight);
-                bundle.putString("date", dogDate);
-                bundle.putBoolean("flag", dogFlagged);
-                bundle.putBoolean("alert", dogAlert);
-                bundle.putString("location", selectedLocation[0]);
-                weightInput.setArguments(bundle);
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container,weightInput).commit();
+                // Handle save button click
+                //TODO: update user in db
             }
         });
 
